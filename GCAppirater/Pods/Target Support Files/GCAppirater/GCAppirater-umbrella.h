e216a841d4b7fe8ad1c736d7e692d19cb1f2d921
bernberg11 <bernberg11@yahoo.com>
2016-03-01 11:25:47 -0500

    Initial set up of XCode project for pod (with sample unit test)

---
+++ GCAppirater/Pods/Target Support Files/GCAppirater/GCAppirater-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "GCAppirater.h"
+#import "GCAppiraterDelegate.h"
+
+FOUNDATION_EXPORT double GCAppiraterVersionNumber;
+FOUNDATION_EXPORT const unsigned char GCAppiraterVersionString[];
+
---
+++ GCAppirater/build/Pods.build/Debug-iphoneos/GCAppirater.build/DerivedSources/GCAppirater_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char GCAppiraterVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:GCAppirater  PROJECT:Pods-1" "\n"; const double GCAppiraterVersionNumber __attribute__ ((used)) = (double)1.;
