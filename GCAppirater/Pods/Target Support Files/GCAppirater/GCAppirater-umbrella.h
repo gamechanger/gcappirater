0742ee142c33ac3426cdc160ab071994d0914117
bernberg11 <bernberg11@yahoo.com>
2016-03-03 14:14:04 -0500

    Add unit tests

--- GCAppirater/Pods/Target Support Files/GCAppirater/GCAppirater-umbrella.h
+++ GCAppirater/Pods/Target Support Files/GCAppirater/GCAppirater-umbrella.h
@@ -1,5 +1,6 @@
 #import <UIKit/UIKit.h>
 
+#import "GCAppirater+Private.h"
 #import "GCAppirater.h"
 #import "GCAppiraterDelegate.h"
 
--- GCAppirater/Tests/GCAppiraterTests.m
+++ GCAppirater/Tests/GCAppiraterTests.m
@@ -10,52 +10,94 @@
 
 #import <Kiwi/Kiwi.h>
 #import <GCAppirater/GCAppirater.h>
+#import <GCAppirater/GCAppirater+Private.h>
 
 SPEC_BEGIN(InitialTests)
 
 describe(@"GCAppirater", ^{
   __block GCAppirater *appirater;
-  beforeAll(^{
+  __block NSDate *currentDate;
+  beforeEach(^{
     appirater = [[GCAppirater alloc] init];
+    [[NSUserDefaults standardUserDefaults] removeObjectForKey:kGCAppiraterFirstUseDate];
+    [[NSUserDefaults standardUserDefaults] removeObjectForKey:kGCAppiraterUseCount];
+    [[NSUserDefaults standardUserDefaults] removeObjectForKey:kGCAppiraterSignificantEventCount];
+    [[NSUserDefaults standardUserDefaults] removeObjectForKey:kGCAppiraterUserRated];
+    [[NSUserDefaults standardUserDefaults] removeObjectForKey:kGCAppiraterUserDeclinedToRate];
+    [[NSUserDefaults standardUserDefaults] removeObjectForKey:kGCAppiraterAskAgainDate];
+    [[NSUserDefaults standardUserDefaults] removeObjectForKey:kGCAppiraterReminderRequestVersion];
+    currentDate = [NSDate date];
+    [NSDate stub:@selector(date) andReturn:currentDate];
   });
   
-  context(@"User has rated app w/o ask again date", ^{
+  describe(@"migrateAppiraterData", ^{
+    __block NSDate *stubDate;
     beforeEach(^{
-      [[NSUserDefaults standardUserDefaults] objectForKey:kGCAppirater]
+      stubDate = [NSDate dateWithTimeIntervalSinceNow:kGCAppiraterTimeIntervalUntilPromptingAgain];
+      [NSDate stub:@selector(dateWithTimeIntervalSinceNow:) andReturn:stubDate withArguments:any()];
     });
-    it(@"will set ask again date", ^{
-      
+    context(@"User has rated app w/o ask again date", ^{
+      beforeEach(^{
+        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kGCAppiraterUserRated];
+      });
+      it(@"will set ask again date", ^{
+        [appirater migrateAppiraterData];
+        NSDate *rateDate = [[NSUserDefaults standardUserDefaults] objectForKey:kGCAppiraterAskAgainDate];
+        [[rateDate should] equal:stubDate];
+      });
       
     });
     
+    context(@"User has declined rating app w/o ask again date", ^{
+      beforeEach(^{
+        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kGCAppiraterUserDeclinedToRate];
+      });
+      it(@"will set ask again date", ^{
+        [appirater migrateAppiraterData];
+        NSDate *rateDate = [[NSUserDefaults standardUserDefaults] objectForKey:kGCAppiraterAskAgainDate];
+        [[rateDate should] equal:stubDate];
+      });
+    });
+    
+    context(@"User has rated but already has ask again date", ^{
+      __block NSDate *askAgainDate;
+      beforeEach(^{
+        askAgainDate = [NSDate date];
+        [[NSUserDefaults standardUserDefaults] setObject:askAgainDate forKey:kGCAppiraterAskAgainDate];
+      });
+      it(@"won't overwrite the ask again date", ^{
+        [appirater migrateAppiraterData];
+        NSDate *rateDate = [[NSUserDefaults standardUserDefaults] objectForKey:kGCAppiraterAskAgainDate];
+        [[rateDate should] equal:askAgainDate];
+      });
+    });
   });
   
-  context(@"will fail", ^{
-
-    
-      it(@"can do maths", ^{
-          [[@1 should] equal:@2];
+  describe(@"userIsEligibleToRate", ^{
+    context(@"No date set for next rating", ^{
+      it(@"returns that user is eligible", ^{
+        BOOL result = [appirater userIsEligibleToRate];
+        [[@(result) should] beTrue];
       });
-
-      it(@"can read", ^{
-          [[@"number" should] equal:@"string"];
+    });
+    context(@"Current date is before Ask Again date", ^{
+      beforeEach(^{
+        [[NSUserDefaults standardUserDefaults] setObject:[NSDate dateWithTimeInterval:100 sinceDate:currentDate] forKey:kGCAppiraterAskAgainDate];
       });
-    
-      it(@"will wait and fail", ^{
-          NSObject *object = [[NSObject alloc] init];
-          [[expectFutureValue(object) shouldEventually] receive:@selector(autoContentAccessingProxy)];
+      it(@"returns that user is not eligible", ^{
+        BOOL result = [appirater userIsEligibleToRate];
+        [[@(result) should] beFalse];
       });
-  });
-
-  context(@"will pass", ^{
-    
-      it(@"can do maths", ^{
-        [[@1 should] beLessThan:@23];
+    });
+    context(@"Current date is after Ask Again date", ^{
+      beforeEach(^{
+        [[NSUserDefaults standardUserDefaults] setObject:[NSDate dateWithTimeInterval:-100 sinceDate:currentDate] forKey:kGCAppiraterAskAgainDate];
       });
-    
-      it(@"can read", ^{
-          [[@"team" shouldNot] containString:@"I"];
-      });  
+      it(@"retuns that user is eligible", ^{
+        BOOL result = [appirater userIsEligibleToRate];
+        [[@(result) should] beTrue];
+      });
+    });
   });
   
 });
